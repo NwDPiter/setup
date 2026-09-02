bd17a0eaddc12e38d7eba8865ca3ce6b638e5625
pedro.sales <pedro.felipe11@hotmail.com>
2026-09-02 17:09:40 -0300

    update:  rebuild  menu

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ read_tty() {
 # ==========================================
 clear
 echo -e "\n\033[1;36m=== pdrops.net Workstation Setup & Management ===\033[0m\n"
-echo "1) Instalar Componentes (Apenas user atual)"
+echo "1) Instalar Componentes"
 echo "2) Remover / Desinstalar Componentes"
 echo "3) Sair"
 
@@ -38,42 +38,71 @@ esac
 while true; do
     clear
     echo -e "\n\033[1;36m=== Seleção de Perfil ===\033[0m\n"
-    echo "1) Combos Customizados (Sub-menu)"
-    echo "2) Perfil Completo (Terminal + CLI + Docker + K8s)"
-    echo "3) Apenas Terminal & Shell (Zsh, Starship, Dotfiles)"
-    echo "4) Apenas Utilitários CLI (FZF, Zoxide, Bat, Ripgrep, Nvim, Byobu)"
-    echo "5) Apenas Stack Containers (Docker, Compose, Lazydocker, Incus)"
-    echo "6) Apenas Stack Kubernetes (kubectl, Helm, K9s, jq, yq)"
-    echo "7) Sair"
 
-    read_tty "\n\033[1;33mEscolha o perfil de ambiente desejado (1-7): \033[0m" PROFILE_CHOICE
+
+    echo "1) Terminal & CLI (Sub-menu)"
+    echo "2) Zabbix & Grafana (Sub-menu)"
+    echo "3) Stack de Orquestração & Containers (sub-menu)"
+    #echo "5) Apenas Utilitários CLI (FZF, Zoxide, Bat, Ripgrep, Nvim, Byobu)"
+    echo "5) Sair"
+
+    read_tty "\n\033[1;33mEscolha o perfil de ambiente desejado (1-5): \033[0m" PROFILE_CHOICE
 
     case "$PROFILE_CHOICE" in
         1)
-            # Sub-menu de Combos
+            # Terminal& Shell + Utilitários CLI
             while true; do
                 clear
-                echo -e "\n\033[1;36m=== Combos Customizados ===\033[0m\n"
-                echo "1) Combo Workstation (Terminal/Shell + Utilitários CLI)"
-                echo "2) Combo DevOps Infra (Containers + Kubernetes)"
-                echo "3) Voltar ao Menu Principal"
+                echo -e "\n\033[1;36m=== Terminal & CLI ===\033[0m\n"
+                echo "1) Apenas Terminal & Shell (Zsh, Starship, Dotfiles)"
+                echo "2) Apenas Utilitários CLI (FZF, Zoxide, Bat, Ripgrep, Nvim, Byobu)"
+                echo "3) Ambos -> (Terminal & Shell + Utilitários CLI)"
+                echo "4) Voltar ao Menu Principal"
 
-                read_tty "\n\033[1;33mEscolha o combo desejado (1-3): \033[0m" COMBO_CHOICE
+                read_tty "\n\033[1;33mEscolha o combo desejado (1-4): \033[0m" COMBO_CHOICE
 
                 case "$COMBO_CHOICE" in
                     1)
                         clear
                         bash <(curl -fsSL "$BASE_URL/scripts/zsh.sh") $MODE_FLAG < /dev/tty
-                        bash <(curl -fsSL "$BASE_URL/scripts/cli.sh") $MODE_FLAG < /dev/tty
                         exit 0
                         ;;
                     2)
                         clear
-                        bash <(curl -fsSL "$BASE_URL/scripts/docker.sh") $MODE_FLAG < /dev/tty
-                        bash <(curl -fsSL "$BASE_URL/scripts/k8s.sh") $MODE_FLAG < /dev/tty
+                        bash <(curl -fsSL "$BASE_URL/scripts/cli.sh") $MODE_FLAG < /dev/tty
                         exit 0
                         ;;
                     3)
+                        clear
+                        bash <(curl -fsSL "$BASE_URL/scripts/zsh.sh") $MODE_FLAG < /dev/tty
+                        bash <(curl -fsSL "$BASE_URL/scripts/cli.sh") $MODE_FLAG < /dev/tty
+                        exit 0
+                        ;;
+                    *)
+                        echo "Opção inválida."
+                        sleep 1
+                        ;;
+                esac
+            done
+            ;;
+            
+        2)
+            while true; do
+                clear
+                echo -e "\n\033[1;36m=== Zabbix & Grafana? ===\033[0m\n"
+                echo "1) Zabbix Agent 2 (Sub-menu)"
+                #echo "2) Instalar Grafana Agent"
+                echo "2) Voltar ao Menu Principal"
+
+                read_tty "\n\033[1;33mEscolha a ação desejada (1-3): \033[0m" ZABBIX_CHOICE
+
+                case "$ZABBIX_CHOICE" in
+                    1)
+                        clear
+                        bash <(curl -fsSL "$BASE_URL/zabbix/agent2.sh") < /dev/tty
+                        exit 0
+                        ;;
+                    2)
                         break # Volta para o laço do Menu Principal
                         ;;
                     *)
@@ -83,19 +112,49 @@ while true; do
                 esac
             done
             ;;
-        2)
-            clear
-            bash <(curl -fsSL "$BASE_URL/scripts/zsh.sh") $MODE_FLAG < /dev/tty
-            bash <(curl -fsSL "$BASE_URL/scripts/cli.sh") $MODE_FLAG < /dev/tty
-            bash <(curl -fsSL "$BASE_URL/scripts/docker.sh") $MODE_FLAG < /dev/tty
-            bash <(curl -fsSL "$BASE_URL/scripts/k8s.sh") $MODE_FLAG < /dev/tty
-            exit 0
+        3) 
+            while true; do
+                clear
+                echo -e "\n\033[1;36m=== Stack de Orquestração & Containers ===\033[0m\n"
+                echo "1) Ambos -> (Docker + Kubernetes)"
+                echo "2) Instalar -> (Docker, Compose, Lazydocker, Incus)"
+                echo "3) Instalar -> (Kubernetes, Helm, K9s, jq, yq)"
+                echo "4) Voltar ao Menu Principal"
+
+                read_tty "\n\033[1;33mEscolha a ação desejada (1-4): \033[0m" STACK_CHOICE
+
+                case "$STACK_CHOICE" in
+                    1)
+                        clear
+                        bash <(curl -fsSL "$BASE_URL/scripts/docker.sh") $MODE_FLAG < /dev/tty
+                        bash <(curl -fsSL "$BASE_URL/scripts/k8s.sh") $MODE_FLAG < /dev/tty
+                        exit 0
+                        ;;
+                    2)
+                        clear
+                        bash <(curl -fsSL "$BASE_URL/scripts/docker.sh") $MODE_FLAG < /dev/tty
+                        exit 0
+                        ;;
+                    3)
+                        clear
+                        bash <(curl -fsSL "$BASE_URL/scripts/k8s.sh") $MODE_FLAG < /dev/tty
+                        exit 0
+                        ;;
+                    4)
+                        break # Volta para o laço do Menu Principal
+                        ;;
+                    *)
+                        echo "Opção inválida."
+                        sleep 1
+                        ;;
+                esac
+            done
             ;;
-        3) clear; bash <(curl -fsSL "$BASE_URL/scripts/zsh.sh") $MODE_FLAG < /dev/tty; exit 0 ;;
+
+
+
         4) clear; bash <(curl -fsSL "$BASE_URL/scripts/cli.sh") $MODE_FLAG < /dev/tty; exit 0 ;;
-        5) clear; bash <(curl -fsSL "$BASE_URL/scripts/docker.sh") $MODE_FLAG < /dev/tty; exit 0 ;;
-        6) clear; bash <(curl -fsSL "$BASE_URL/scripts/k8s.sh") $MODE_FLAG < /dev/tty; exit 0 ;;
-        7) clear; echo "Saindo..."; exit 0 ;;
+        5) clear; echo "Saindo..."; exit 0 ;;
         *) echo "Opção inválida."; sleep 1 ;;
     esac
 done
